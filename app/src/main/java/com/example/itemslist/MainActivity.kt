931051b839e71a8af2aaf3141d4d0805916f3752
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-01-06 20:35:30 +0530

    Dagger hilt setup

--- app/src/main/java/com/example/itemslist/MainActivity.kt
+++ app/src/main/java/com/example/itemslist/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.itemslist.presentation.navigation.NavGraph
 import com.example.itemslist.ui.theme.ItemsListTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
